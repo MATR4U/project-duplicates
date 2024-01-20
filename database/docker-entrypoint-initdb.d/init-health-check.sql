ed305ca76b565d6e43ef0c3deed040c09da40dc6
doc1024 <github-sstraube@straube.ch>
2024-01-21 00:47:59 +0100

    Fix DB health check at startup

--- database/docker-entrypoint-initdb.d/init-health-check.sql
+++ database/docker-entrypoint-initdb.d/init-health-check.sql
@@ -1,9 +1,14 @@
 CREATE TABLE health_check (
-    service_id PRIMARY KEY,
+    service_id SERIAL PRIMARY KEY,
     service_name VARCHAR(255) NOT NULL,
     timestamp TIMESTAMP NOT NULL,
     status VARCHAR(255) NOT NULL
 );
 
-INSERT INTO health_check (timestamp, status)
-VALUES (1, "db_service", '2023-12-21 10:00:00', 'OK');
+INSERT INTO health_check (service_id, service_name, timestamp, status)
+VALUES (1, 'db_service', CURRENT_TIMESTAMP, 'OK')
+ON CONFLICT (service_id)
+DO UPDATE SET
+    service_name = EXCLUDED.service_name,
+    timestamp = CURRENT_TIMESTAMP,
+    status = EXCLUDED.status;
